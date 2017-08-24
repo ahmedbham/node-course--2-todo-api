7ea1ca0980b18d3358c375ae29d46332d406a12e
ahmed bham <ahmed.bham@gmail.com>
2017-08-24 17:48:07 -0400

    added delete file

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+// var obj = new ObjectID()
+// console.log(obj)
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) return console.log('unable to connecto mongodb server')
+  console.log('connected to mongodb')
+
+  db.collection('Todos').findOneAndDelete({completed: false}).then((results) => {
+     console.log(results)})
+
+  // db.collection('Todos').deleteOne({text: 'some things'}).then((results) => {
+  //   console.log(results)})
+
+  // db.collection('Todos').deleteMany({text: 'eat lunch'}).then((results) => {
+  //   console.log(results)})
+
+// db.collection('Users').find({name: 'ahmed bham'}).toArray().then((docs) => {
+//   console.log('Users')
+//   console.log(JSON.stringify(docs, undefined, 2))
+// }, (err) => {
+//   console.log('unable to fetch documents', err)
+// })
+//
+// db.collection('Users').find({name: 'ahmed bham'}).count().then((count) => {
+//   console.log(`Todos: ${count}`)
+// }, (err) => {
+//   console.log('unable to fetch documents', err)
+// })
+
+//  db.close()
+})
